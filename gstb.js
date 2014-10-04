a4c8bd61142c9cd568415d156c226e8aaa7325fd
clux <analsandblaster@gmail.com>
2014-10-04 17:43:01 +0100

    this is how it SHOULD look - i think (using module ase API desirability testing ground)

--- gstb.js
+++ gstb.js
@@ -1,131 +1,68 @@
 var GroupStage = require('groupstage');
 var TieBreaker = require('tiebreaker');
-var $ = require('autonomy');
+var Tourney = require('tourney');
 
-function GroupStageTb(numPlayers, opts) {
-  opts = GroupStageTb.defaults(numPlayers, opts);
-  var invReason = GroupStageTb.invalid(numPlayers, opts);
-  if (invReason !== null) {
-    console.error("Invalid %d player GroupStageTb with opts=%j rejected",
-      numPlayers, opts
-    );
-    throw new Error("Cannot construct GroupStageTb: " + invReason);
-  }
-  this.current = new GroupStage(numPlayers, opts); // make private
+var GsTb = Tourney.sub('GroupStage-Tb', function (opts, initParent) {
   this.limit = opts.limit;
-  this.stage = 1;
-  this.matches = []; // since this is a tourney - need to ask for this.active()
-}
+  initParent(new GroupStage(this.numPlayers, opts));
+});
 
-
-GroupStageTb.invalid = function (np, opts) {
-  var o = GroupStageTb.defaults(np, opts);
-  var invReason = GroupStage.invalid(np, o);
-  if (invReason !== null) {
-    return invReason;
-  }
-  if (o.limit <= 0) {
-    return "need to specify a non-zero limit";
-  }
-  var numGroups = Math.ceil(np / o.groupSize);
-  if (o.limit % numGroups !== 0) {
-    return "number of groups must divide limit";
-  }
-  return null;
-};
-GroupStageTb.defaults = function (np, opts) {
-  var o = GroupStage.defaults(np, opts);
-  o.limit = opts.limit | 0; // can't really guess this - force users to think
-  return o;
-};
-
-
-// proxy a few methods onto current
-GroupStageTb.prototype.score = function (id, score) {
-  return this.current.score(id, score);
-};
-GroupStageTb.prototype.unscorable = function (id, score, allowPast) {
-  return this.current.unscorable(id, score, allowPast);
-};
-GroupStageTb.prototype.results = function () {
-  return this.current.results();
-};
-
-// TODO: maybe freeze/unfreeze matches so only score modifies it
-GroupStageTb.prototype.active = function () {
-  return this.current.matches.slice(); // old-style matches
-};
-
-GroupStageTb.prototype.players = function (id) {
-  if (!id.t || id.t === this.stage) {
-    return this.current.players(id);
-  }
-  // TODO: else look through old matches ?
-};
-
-function Id(t, s, r, m) {
-  this.t = t;
-  this.s = s;
-  this.r = r;
-  this.m = m;
-}
-Id.prototype.toString = function () {
-  return "T" + this.t + " G" + this.s + " R" + this.r + " M" + this.m;
-};
-
-var storeHistory = function (ms, extra, stage) {
-  extra.forEach(function (m) {
-    var copy = {
-      id: new Id(stage, m.id.s, m.id.r, m.id.m),
-      p: m.p.slice()
-    };
-    if (m.m) {
-      copy.m = m.m.slice();
+GsTb.configure({
+  defaults: function (np, opts) {
+    opts.limit = opts.limit | 0;
+    return opts;
+  },
+  invalid: function (np, opts) {
+    var invReason = GroupStage.invalid(np, opts);
+    if (invReason !== null) {
+      return invReason;
     }
-    ms.push(m);
-  });
+    if (opts.limit <= 0) {
+      return "need to specify a non-zero limit";
+    }
+    var numGroups = Math.ceil(np / opts.groupSize);
+    if (opts.limit % numGroups !== 0) {
+      return "number of groups must divide limit";
+    }
+    return null;
+  }
+});
+
+//------------------------------------------------------------------
+// Stage identifiers
+//------------------------------------------------------------------
+
+GsTb.prototype.inGroupStage = function () {
+  return this._inst.name === 'GroupStage';
 };
 
-GroupStageTb.prototype.isStageComplete = function () {
-  return this.current.isDone();
+GsTb.prototype.inTieBreaker = function () {
+  return this._inst.name === 'TieBreaker';
 };
 
-GroupStageTb.prototype._mustPropagate = function () {
+//------------------------------------------------------------------
+// Expected methods
+//------------------------------------------------------------------
+
+GsTb.prototype._mustPropagate = function () {
   return TieBreaker.isNecessary(this.current, this.limit);
 };
 
-GroupStageTb.prototype.propagate = function () {
-  if (!this.isStageComplete() || !this._mustPropagate()) {
-    return false;
-  }
-  // TODO: keep track of old instances and matches...
-  var tb = TieBreaker.from(this.current, this.limit, { grouped: true });
-  storeHistory(this.matches, this.current.matches, this.stage);
-  this.current = tb;
-  this.stage += 1;
-  return true;
+GsTb.prototype._createNext = function () {
+  // inst is GroupStage or TieBreaker but solution is always the same:
+  return TieBreaker.from(this.current, this.limit, { grouped: true });
 };
 
-GroupStageTb.prototype.isDone = function () {
-  return this.current.isDone() && !this._mustPropagate();
+//------------------------------------------------------------------
+// Overrides
+//------------------------------------------------------------------
+
+GsTb.prototype.results = function () {
+  // if inGroupStage - _inst.results() are correct
+  // else TieBreaker has updated previous results it was fed
+  return this._inst.results();
 };
 
-GroupStageTb.prototype.isTieBreakerRound = function () {
-  return this.current.name === 'TieBreaker';
-};
+//------------------------------------------------------------------
 
-GroupStageTb.prototype.upcoming = function (playerId) {
-  if (!this.isStageComplete()) {
-    var partial = this.current.upcoming(playerId);
-    return partial ? $.extend({ t: this.stage }, partial) : undefined;
-  }
-  // NB: do NOT figure out id by creating deterministic tiebreaker round early
-  // if we gave out the ID, any 'link' to such a match would fail to exist yet
-
-  // although this kind of implies a policy:
-  // never guess upcoming between stages
-  // which sounds sensible - maybe some stage creation is parametrized..
-};
-
-
-module.exports = GroupStageTb;
+module.exports = GsTb;
